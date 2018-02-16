b89e65405bc2d02a21e01c4c33fcee1b7aba3f15
tabby42 <irene.theiss@gmx.at>
2018-02-16 18:39:51 +0100

    move markup for displaying car list to function

--- car_list.php
+++ car_list.php
@@ -1,14 +1,8 @@
 <?php 
 $pagename = "Cars";
 include "partials/header.php";
-
-
 //get car list
 $cars = new CarList();
-if (!empty($_GET['q'])) {
-	$filter = $_GET['q'];
-	var_dump($filter);
-}
 $cars->carList = $cars->getCars();
 //var_dump($cars->carList);
 //get office list
@@ -30,38 +24,7 @@ $offices->officeList = $offices->getOffices();
 		  		</form>
 		  	</div>
   			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false">
-  				<?php foreach ($cars->carList as $value): ?>
-					<div class="panel panel-default">
-					   	<div class="panel-heading">
-				   			<h4 class="panel-title">
-					        	<?php echo $value->brand . " | " . $value->cartype;?>
-					        		<?php if ($value->office !== null): ?>
-					        			<label class="label label-info">Available</label>
-					        		<?php else: ?>
-					        			<label class="label label-warning">On the road</label>
-					        		<?php endif ?>
-					        		<label class="label label-primary pull-right"><?php echo $value->pricePerDay . " €/Day";?></label>
-					        	<a href="#" class="" data-toggle="collapse" data-target="#collapsible-<?php echo $value->id;?>" data-parent="#myAccordion"> More
-					        		<span class=""><i class="fas fa-caret-down"></i></span>
-						        </a>
-						   	</h4>
-					   	</div>
-				        <div id="collapsible-<?php echo $value->id;?>" class="collapse">
-				        	<div class="panel-body">
-				        		<div class="col-xs-6">
-				        			<span class="badge"><?php echo $value->ps . " PS";?></span>
-					        		<span class="badge"><?php echo $value->circuit;?></span>
-					        		<span class="badge"><i class="far fa-snowflake"></i><?php echo $value->aircondition;?></span>
-					        		<span class="badge"><?php echo $value->numSeats . " | " . $value->numDoors;?></span>
-				        		</div>
-				        		<div class="col-xs-6">
-				        			<?php echo $value->office;?><br>
-				        			<?php echo $value->coordinates;?><br>
-				        		</div>
-				        	</div>
-				        </div>
-				   </div>
-				<?php endforeach ?>
+  				<?php echo $cars->displayCars();?>
   			</div>
   		</div><!--/content column -->
   		<div class="col-md-2 hidden-xs"></div>
