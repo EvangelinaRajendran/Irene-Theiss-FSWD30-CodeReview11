d089fc3f1fa249c7b3173aaf65fae2079a2d8bea
tabby42 <irene.theiss@gmx.at>
2018-02-16 18:38:21 +0100

    add ajax call for filtering car list

--- js/script.js
+++ js/script.js
@@ -3,26 +3,21 @@ jQuery(document).ready(function($) {
 	$('#select-offices').on('change', function() {
 		var filter = $(this).val();
 		var field = $(this).attr('name');
-		console.log(field);
-
-		if (filter === "All") {
-			return;
-		} else {
-			if (window.XMLHttpRequest) {
-	            // code for IE7+, Firefox, Chrome, Opera, Safari
-	            xmlhttp = new XMLHttpRequest();
-	        } else {
-	            // code for IE6, IE5
-	            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	        }
-	        xmlhttp.onreadystatechange = function() {
-	            if (this.readyState == 4 && this.status == 200) {
-					console.log(this.responseText);
-	                //document.getElementById("txtHint").innerHTML = this.responseText;
-	            }
-	        };
-	        xmlhttp.open("GET","aj_test.php?q=" + filter + "&f=" + field, true);
-	        xmlhttp.send();
-		}
+		//console.log(field);
+		if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+				console.log(this.responseText);
+                document.getElementById("accordion").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","filter_cars.php?q=" + filter + "&f=" + field, true);
+        xmlhttp.send();
 	});
 });
