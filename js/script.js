a4ae3fbeca0a4de48d8861f3a9e5b4ee5c79826b
tabby42 <irene.theiss@gmx.at>
2018-02-17 10:28:53 +0100

    modify car maps function

--- js/script.js
+++ js/script.js
@@ -37,10 +37,10 @@ jQuery(document).ready(function($) {
 	  		scaledSize: new google.maps.Size(30, 30), // scaled size
 	  	};
 	    for (i = 0; i < locations.length; i++) {  
-	      marker = new google.maps.Marker({
-	        position: new google.maps.LatLng(locations[i][0], locations[i][1]),
-	        map: map,
-	        icon: image
+	      	marker = new google.maps.Marker({
+		        position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+		        map: map,
+		        icon: image
 	      });
 
 	      google.maps.event.addListener(marker, 'click', (function(marker, i) {
@@ -61,34 +61,40 @@ jQuery(document).ready(function($) {
     //individual car maps
     //console.log(carmarkers);
     function initCarMaps() {
-    	var maps = [];
 	  	var locations = carmarkers;
 	  	var wrappers = document.getElementsByClassName('map-container');
-
-	  	for (var i = 0; i < wrappers.length; i++) {
-
+		var marker, i;
+	  	var infowindow = new google.maps.InfoWindow();
+	  	var image = {
+	  		url: "img/carmarker.png",
+	  		scaledSize: new google.maps.Size(30, 30), // scaled size
+	  	};
+	  	//append map to each car element
+	  	for (i = 0; i < wrappers.length; i++) {
 	  		var latlng = {lat: parseFloat(locations[i][0]), lng: parseFloat(locations[i][1])};
 	  		//console.log(latlng);
 	  		var mapOptions = {
 	           center: latlng,
-	           scrollwheel: false,
-	           zoom: 12
+	           scrollwheel: true,
+	           zoom: 15
 	       };
 	       var map = new google.maps.Map(document.getElementById('map-' + (i + 1)), mapOptions);
-       	   maps.push(map);
-       	   console.log(maps);
        	   var marker = new google.maps.Marker({
 	          map: map,
 	          position: latlng,
-	          title: locations[i][0]
+	          icon: image
 	       });
+	       google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		        return function() {
+		          infowindow.setContent(locations[i][2] || "On the road");
+		          infowindow.open(map, marker);
+		        }
+	        })(marker, i));
 	  	}
-
     }
-	//initialize car map only on car page
+	//initialize car maps only on car page
     if( $('.map-container').length > 0) {
 		google.maps.event.addDomListener(window, "load", initCarMaps);
-		//initCarMaps();
     }
 
 });
